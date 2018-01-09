05e6463a5ad3fcfc3c2ef780b8155a01405780cd
xinhao_han <1500252978@qq.com>
2018-01-09 17:10:02 +0800

    绚丽动画版本

---
+++ app/src/main/java/com/example/a14178/xhwateview/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a14178.xhwateview;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private XHWaveView image;
+    private SeekBar pb;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.image);
+
+        pb = findViewById(R.id.pb);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
+        pb.setMax(displayMetrics.heightPixels);
+        pb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                image.setIndex(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
